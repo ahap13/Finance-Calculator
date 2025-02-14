98368074cdc134eeedeede20a4808928b0c14da8
ahap13 <102983378+ahap13@users.noreply.github.com>
2025-02-14 05:49:37 -0500

    Create calculator.py
    initial commit

---
+++ calculator.py
@@ -0,0 +1,16 @@
+
+"""Each function will take amount owed, interest rate, minimum payment, and total amount to be applied monthly to debt.
+
+All should take the above, specifics below:
+One function will take the amount of CCs, APR for each, and return a pay off plan (maybe snowball vs avalanche?)
+One function will calculate auto loan
+One function will calculate mortgage amount
+
+All return a graph of payoff over time, with one line including only minimum payments and one line being extra payment track"""
+
+# Debt object?
+    # Interest type
+    # Interest rate
+    # Minimum Payment
+    # Amount owed
+    # Make payment
